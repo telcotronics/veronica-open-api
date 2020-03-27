c301dd4b5552e296d62753242db70136df6e892e
shoniisra <villajohnny@gmail.com>
2020-03-27 05:46:04 -0500

    adding Files PurchaseClearance

--- app/src/main/java/com/rolandopalermo/facturacion/ec/app/config/MapperConfig.java
+++ app/src/main/java/com/rolandopalermo/facturacion/ec/app/config/MapperConfig.java
@@ -3,6 +3,7 @@ package com.rolandopalermo.facturacion.ec.app.config;
 import com.rolandopalermo.facturacion.ec.dto.v1.bol.GuiaRemisionDTO;
 import com.rolandopalermo.facturacion.ec.dto.v1.cm.NotaCreditoDTO;
 import com.rolandopalermo.facturacion.ec.dto.v1.dm.NotaDebitoDTO;
+import com.rolandopalermo.facturacion.ec.dto.v1.pc.LiquidacionCompraDTO;
 import com.rolandopalermo.facturacion.ec.dto.v1.invoice.FacturaDTO;
 import com.rolandopalermo.facturacion.ec.dto.v1.withholding.RetencionDTO;
 import com.rolandopalermo.facturacion.ec.jaxb.impl.v1_0.SRIResponseJaxbServiceImpl;
@@ -10,6 +11,7 @@ import com.rolandopalermo.facturacion.ec.mapper.Mapper;
 import com.rolandopalermo.facturacion.ec.mapper.bol.GuiaRemisionMapper;
 import com.rolandopalermo.facturacion.ec.mapper.cm.NotaCreditoMapper;
 import com.rolandopalermo.facturacion.ec.mapper.dm.NotaDebitoMapper;
+import com.rolandopalermo.facturacion.ec.mapper.pc.LiquidacionCompraMapper;
 import com.rolandopalermo.facturacion.ec.mapper.invoice.FacturaMapper;
 import com.rolandopalermo.facturacion.ec.mapper.sri.RespuestaComprobanteMapper;
 import com.rolandopalermo.facturacion.ec.mapper.sri.RespuestaSolicitudMapper;
@@ -19,6 +21,7 @@ import com.rolandopalermo.facturacion.ec.modelo.guia.GuiaRemision;
 import com.rolandopalermo.facturacion.ec.modelo.notacredito.NotaCredito;
 import com.rolandopalermo.facturacion.ec.modelo.notadebito.NotaDebito;
 import com.rolandopalermo.facturacion.ec.modelo.retencion.ComprobanteRetencion;
+import com.rolandopalermo.facturacion.ec.modelo.liquidacion.LiquidacionCompra;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -43,6 +46,11 @@ public class MapperConfig {
     @Bean
     public Mapper<FacturaDTO, Factura> getInvoiceMapper() {
         return new FacturaMapper();
+    }
+
+    @Bean
+    public Mapper<LiquidacionCompraDTO, LiquidacionCompra> getPurchaseClearanceMapper() {
+        return new PurchaseClearanceMapper();
     }
 
     @Bean
---
+++ service/src/main/java/com/rolandopalermo/facturacion/ec/service/sri/v1/PurchaseClearanceServiceImp.java
@@ -0,0 +1,29 @@
+package com.rolandopalermo.facturacion.ec.service.sri.v1;
+
+import com.rolandopalermo.facturacion.ec.common.DateUtils;
+import com.rolandopalermo.facturacion.ec.common.exception.VeronicaException;
+import com.rolandopalermo.facturacion.ec.domain.PurchaseClearance;
+import com.rolandopalermo.facturacion.ec.dto.v1.pc.LiquidacionCompraDTO;
+import com.rolandopalermo.facturacion.ec.modelo.liquidacion.LiquidacionCompra;
+import org.springframework.stereotype.Service;
+
+import static org.exolab.castor.dsml.ImportEventListener.CREATED;
+
+@Service("purchaseClearanceServiceImpl")
+public class PurchaseClearanceServiceImpl extends GenericSRIServiceImpl<LiquidacionCompraDTO, LiquidacionCompra, PurchaseClearance> {
+
+    @Override
+    public PurchaseClearance toEntity(LiquidacionCompra liquidacionCompra, String contentAsXML) throws VeronicaException {
+        PurchaseClearance purchaseClearance = new PurchaseClearance();
+        purchaseClearance.setAccessKey(liquidacionCompra.getInfoTributaria().getClaveAcceso());
+        purchaseClearance.setSriVersion(liquidacionCompra.getVersion());
+        purchaseClearance.setXmlContent(contentAsXML);
+        purchaseClearance.setSupplierId(liquidacionCompra.getInfoTributaria().getRuc());
+        purchaseClearance.setCustomerId(liquidacionCompra.getInfoLiquidacionCompra().getIdentificacionComprador());
+        purchaseClearance.setIssueDate(DateUtils.getFechaFromStringddMMyyyy(liquidacionCompra.getInfoLiquidacionCompra().getFechaEmision()));
+        purchaseClearance.setPurchaseClearanceNumber(liquidacionCompra.getInfoTributaria().getSecuencial());
+        purchaseClearance.setInternalStatusId(CREATED);
+        return purchaseClearance;
+    }
+
+}
